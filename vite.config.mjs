20306ae7322b215c56bd33daa025bfb4e53f4b53
Víctor_3.1416 <matemorales2020@gmail.com>
2025-11-04 09:30:09 -0600

    agregando docs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Cadenas_Markov/'
+  base: '/Cadenas_Markov/',  // 👈 igual que antes
+  build: {
+    outDir: 'docs'        // 👈 genera el build en /docs
+  }
 })
